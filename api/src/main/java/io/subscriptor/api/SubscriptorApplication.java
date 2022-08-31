8f700780e24cd154e2e4a6c7a7f2e1108a45d6aa
Renan Zulian <renanzulian@gmail.com>
2022-08-30 21:00:28 -0300

    chore: add api project

---
+++ api/src/main/java/io/subscriptor/api/SubscriptorApplication.java
@@ -0,0 +1,13 @@
+package io.subscriptor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptorApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/io/subscriptor/api/SubscriptorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.subscriptor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
